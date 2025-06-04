827f1344011cc52e0e01c9484beb7cb109167fd4
Jack Luo <jackluo@5e:ff:6b:6c:a9:bb.home>
2025-06-03 20:35:40 -0400

    fix: bind Flask to 0.0.0.0 and use PORT

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import requests
 from bs4 import BeautifulSoup
 import re
+import os
 
 app = Flask(__name__)
 
@@ -33,4 +34,5 @@ def index():
     return render_template('index.html', title=title, content=content)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
